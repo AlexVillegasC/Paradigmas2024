13275e7e94c4f6560b782d6d62b630326341244f
KarlaAnguloC <angulokarla1201@gmail.com>
2024-09-20 18:09:30 -0600

    Sentry

--- Api.Productos/Program.cs
+++ Api.Productos/Program.cs
@@ -5,8 +5,32 @@ using ChatBot_Test.Mappings;
 using ChatBot_Test.Middlewares;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
+using Sentry.Profiling;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.WebHost.UseSentry(o =>
+{
+    o.Dsn = "https://49f6f404ff585a7e33a7023cff88bb5d@o4507909124784128.ingest.us.sentry.io/4507909130289152";
+    // When configuring for the first time, to see what the SDK is doing:
+    o.Debug = true;
+    // Set TracesSampleRate to 1.0 to capture 100%
+    // of transactions for tracing.
+    // We recommend adjusting this value in production
+    o.TracesSampleRate = 1.0;
+    // Sample rate for profiling, applied on top of othe TracesSampleRate,
+    // e.g. 0.2 means we want to profile 20 % of the captured transactions.
+    // We recommend adjusting this value in production.
+    o.ProfilesSampleRate = 1.0;
+    // Requires NuGet package: Sentry.Profiling
+    // Note: By default, the profiler is initialized asynchronously. This can
+    // be tuned by passing a desired initialization timeout to the constructor.
+    o.AddIntegration(new ProfilingIntegration(
+        // During startup, wait up to 500ms to profile the app startup code.
+        // This could make launching the app a bit slower so comment it out if you
+        // prefer profiling to start asynchronously.
+        TimeSpan.FromMilliseconds(500)));
+});
 
 
 builder.Services.AddDbContext<ProductsContext>(options =>
